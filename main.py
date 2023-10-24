0b6f22853c55ce3cf80bfb2d6c03f5077f7a0845
senthil100695 <senthil100695@gmail.com>
2023-10-24 07:40:40 +0530

    data validation added

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from textClassification.exception import AppException
 import sys
 
 from textClassification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-#from textClassification.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from textClassification.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 #from textClassification.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 #from textClassification.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 #from textClassification.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
@@ -13,6 +13,16 @@ STAGE_NAME = "Data Ingestion stage"
 try:
    logging.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logging.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        AppException(e,sys)
+        raise e
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logging.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
    data_ingestion.main()
    logging.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
---
+++ src/textClassification/pipeline/stage_02_data_validation.py
@@ -0,0 +1,14 @@
+from textClassification.config.configuration import ConfigurationManager
+from textClassification.components.data_validation import DataValiadtion
+from textClassification.logging import logging
+
+
+class DataValidationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_validation_config = config.get_data_validation_config()
+        data_validation = DataValiadtion(config=data_validation_config)
+        data_validation.validate_all_files_exist()
